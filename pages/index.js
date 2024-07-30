2376236e6c807b9f0cd24a26031cbe155f90a690
David Andres Sanchez Umbarila <sanchezdavid481@gmail.com>
2024-07-29 22:05:20 -0400

    Initial SetUp

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Next.js!</h1>
+            <p>Get started by editing</p>
+        </div>
+    );
+};
+
+export default Home;
